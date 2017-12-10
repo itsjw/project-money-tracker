75ba5317b8c21a6b63a58adaaf7722a0da82bf88
Leonardo <leonardo.capoccia@hotmail.it>
2017-12-10 17:52:53 +0100

    Add component for project details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { HomeComponent } from './components/home/home.component';
+import { ItemContentComponent } from './components/item-content/item-content.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
     {
         path: '',
-        component: HomeComponent
+        component: HomeComponent,
+        children: [
+            {
+                path: ':projectId',
+                component: ItemContentComponent
+            }
+        ]
     }
 ];
 
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router) { }
 
   ngOnInit() {
+    this._router.navigateByUrl('/1');
   }
 
 }
